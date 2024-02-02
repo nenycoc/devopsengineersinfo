0f91981b55f4257fe730c95733780cbb66ab0ae4
nenycoc <naafn2004@gmail.com>
2024-02-02 15:07:05 +0000

    my first commit

---
+++ src/main/java/com/jendareytech/devopsengineersinfo/DevopsengineersinfoApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.devopsengineersinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsengineersinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsengineersinfoApplication.class, args);
+	}
+
+}
